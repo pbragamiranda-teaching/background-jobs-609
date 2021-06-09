50603ca2960c17eee5501d82868203d28b5c3108
Pedro Miranda <pbragamiranda@gmail.com>
2021-06-09 10:03:55 -0300

    sidekiq

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  after_commit :async_update, on: [:create, :update] # Run on create & update
+
+  private
+
+  def async_update
+    UpdateUserJob.perform_later(self.id)
+  end
 end
